4e3f907d5b6ee4020963983f5488776e6fa21f4e
Vaishnavi Kesherwani <122589971+Vaishu7605@users.noreply.github.com>
2024-08-12 19:48:58 +0530

    a

---
+++ connect/src/main/java/com/example/demo/student.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table
+public class student {
+	@Id
+	
+	@Column
+	private String name;
+	@Column
+	private String city;
+	@Column(name="E-MAIL")
+	private String mail;
+
+}
